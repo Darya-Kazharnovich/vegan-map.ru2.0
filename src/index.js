3385a52ac20aa02e8f329215809d65e25693e3a8
Darya Kazharnovich <you@example.comdarya.kazharnovich@gmail.com>
2023-05-22 15:51:39 +0300

    fix: email

--- src/index.js
+++ src/index.js
@@ -16,29 +16,20 @@ FAQs.addEventListener('click', openTheAnswer)
 window.addEventListener('scroll', scrollFunction)
 toUpButton.addEventListener('click', topFunction)
 
-// window.addEventListener("load", function() {
-//     const form = document.getElementById('email-form');
-//     form.addEventListener("submit", function(e) {
-//       e.preventDefault();
-//       const data = new FormData(form);
-//       const action = form.action;
-//       console.log(action)
-//       fetch(action, {
-//         method: 'POST',
-//         body: data,
-//       })
-//       .then(() => {
-//         alert("Success!");
-//       })
-//     });
-//   });
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyLgJ2Xh5_a_JnvX-T-uks5E33rxEFjAV75PuhQDa0wcv9Gy2Q4yqZ8RjPad8QQJ-fCww/exec'
-const form = document.forms['email-form']
+const form = document.getElementById('email-form')
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', async e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    await fetch(scriptURL, { 
+    method: 'POST',
+    mode: 'no-cors',
+    body: new FormData(form)})
     .then(response => console.log('Success!', response))
     .catch(error => console.error('Error!', error.message))
 })
+
+
+// email: d@mail.ru
+// body: `email : ${await document.getElementById('email').value}`})
