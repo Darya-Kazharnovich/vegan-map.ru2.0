748c6a8407f790b86dcbdd7a3b8f91d63115fb08
Darya Kazharnovich <you@example.comdarya.kazharnovich@gmail.com>
2023-05-25 15:49:22 +0300

    fix: delete plus

--- src/scripts/answer.js
+++ src/scripts/answer.js
@@ -7,13 +7,13 @@ export function openTheAnswer (event) {
     if (event.target.parentNode.classList.contains('FAQ_1') && !event.target.classList.contains('FAQ__answer_open')) {
         answerFAQ1.classList.toggle('FAQ__answer_open')
         answerFAQ1.classList.contains('FAQ__answer_open')? document.querySelector('.plus_1').innerHTML = `-` : document.querySelector('.plus_1').innerHTML = `+`
-    } else if (event.target.parentNode.classList.contains('FAQ_2')) {
+    } else if (event.target.parentNode.classList.contains('FAQ_2') && !event.target.classList.contains('FAQ__answer_open')) {
         answerFAQ2.classList.toggle('FAQ__answer_open')
         answerFAQ2.classList.contains('FAQ__answer_open')? document.querySelector('.plus_2').innerHTML = `-` : document.querySelector('.plus_2').innerHTML = `+`
-    } else if (event.target.parentNode.classList.contains('FAQ_3')) {
+    } else if (event.target.parentNode.classList.contains('FAQ_3') && !event.target.classList.contains('FAQ__answer_open')) {
         answerFAQ3.classList.toggle('FAQ__answer_open')
         answerFAQ3.classList.contains('FAQ__answer_open')? document.querySelector('.plus_3').innerHTML = `-` : document.querySelector('.plus_3').innerHTML = `+`
-    } else if (event.target.parentNode.classList.contains('FAQ_4')) {
+    } else if (event.target.parentNode.classList.contains('FAQ_4') && !event.target.classList.contains('FAQ__answer_open')) {
         answerFAQ4.classList.toggle('FAQ__answer_open')
         answerFAQ4.classList.contains('FAQ__answer_open')? document.querySelector('.plus_4').innerHTML = `-` : document.querySelector('.plus_4').innerHTML = `+`
     }
